df1a0236ebd4e02b46db712d2d0011d86cc2ab25
ithevbro <ithevbro@gmail.com>
2024-03-10 14:37:38 +0200

    final

--- public/js/index.js
+++ public/js/index.js
@@ -34,47 +34,52 @@ shuffle.addEventListener('click', () => {
 function shotSound() {
     let shot = document.getElementById('shot')
     if (shot.paused) {
+        shot.currentTime = 0
         shot.play()
     } else {
-        shot.pause()
         shot.currentTime = 0
         shot.play()
     }
 }
 
 function shipThink() {
-    let shipThink = document.getElementById('shipThink')
-    if (shipThink.paused) {
-        document.getElementById('shot').pause()
-        shipThink.play()
-    } else {
-        document.getElementById('shot').pause()
-        shipThink.pause()
-        shipThink.currentTime = 0
-        shipThink.play()
+    if (document.getElementById('shot').play()) {
+        document.getElementById('shot').play().then(_ => {
+            document.getElementById('shot').pause()
+            document.getElementById('shot').currentTime = 0
+            let shipThink = document.getElementById('shipThink')
+            if (shipThink.paused) {
+                shipThink.play()
+            } else {
+                shipThink.currentTime = 0
+                shipThink.play()
+            }
+        })
     }
+
 }
 
 function gameHandler(e) {
     let ship = e.target.closest('td')
     let count = 0
-    if (!ship || ship.classList.contains('kill') || ship.classList.contains('empty') || queue % 2 !== 0 || gg) {
+    if (!ship || ship.classList.contains('kill') || ship.classList.contains('empty') || queue % 2 !== 0 || gg || ship.classList.contains('emptyhit')) {
         return
     }
     table1stop.style.width = '100%'
     let row = +ship.dataset.ship[0]
     let cell = +ship.dataset.ship[2]
+    shotSound()
     if (coords[row][cell] == 1) {
         socket.emit('move', 'hitt');
         socket.emit('shot', ship.dataset.ship);
-        shotSound()
         coords[row][cell] = 'x'
         ship.className = 'kill'
+
 
     } else {
         socket.emit('move', 'miss');
         socket.emit('shot', ship.dataset.ship);
-        ship.className = 'empty'
+        ship.className = 'emptyhit'
     }
     for (let i = 0; i < shipsLocation.length; i++) {
         let ships = Object.values(shipsLocation[i])
@@ -98,58 +103,57 @@ function gameHandler(e) {
                 let firstcell = +ship[0][2] - 1
                 table2.rows[row].cells[cell].style.backgroundColor = 'rgb(190, 6, 6,0.3)'
                 if (position) {
-                    if (cell + 1 < 10) {
+                    if (cell + 1 < 10 && !table2.rows[row].cells[cell + 1].classList.contains('emptyhit')) {
                         table2.rows[row].cells[cell + 1].className = 'empty'
                     }
-                    if (cell - 1 >= 0) {
+                    if (cell - 1 >= 0 && !table2.rows[row].cells[cell - 1].classList.contains('emptyhit')) {
                         table2.rows[row].cells[cell - 1].className = 'empty'
                     }
-                    if (lastrow < 10) {
+                    if (lastrow < 10 && !table2.rows[lastrow].cells[cell].classList.contains('emptyhit')) {
                         table2.rows[lastrow].cells[cell].className = 'empty'
                     }
-                    if (firstrow >= 0) {
+                    if (firstrow >= 0 && !table2.rows[firstrow].cells[cell].classList.contains('emptyhit')) {
                         table2.rows[firstrow].cells[cell].className = 'empty'
                     }
-                    if (cell - 1 >= 0 && lastrow < 10) {
+                    if (cell - 1 >= 0 && lastrow < 10 && !table2.rows[lastrow].cells[cell - 1].classList.contains('emptyhit')) {
                         table2.rows[lastrow].cells[cell - 1].className = 'empty'
                     }
-                    if (cell + 1 < 10 && lastrow < 10) {
+                    if (cell + 1 < 10 && lastrow < 10 && !table2.rows[lastrow].cells[cell + 1].classList.contains('emptyhit')) {
                         table2.rows[lastrow].cells[cell + 1].className = 'empty'
                     }
-                    if (cell - 1 >= 0 && firstrow >= 0) {
+                    if (cell - 1 >= 0 && firstrow >= 0 && !table2.rows[firstrow].cells[cell - 1].classList.contains('emptyhit')) {
                         table2.rows[firstrow].cells[cell - 1].className = 'empty'
                     }
-                    if (cell + 1 < 10 && firstrow >= 0) {
+                    if (cell + 1 < 10 && firstrow >= 0 && !table2.rows[firstrow].cells[cell + 1].classList.contains('emptyhit')) {
                         table2.rows[firstrow].cells[cell + 1].className = 'empty'
                     }
                 } else {
-                    if (row + 1 < 10) {
+                    if (row + 1 < 10 && !table2.rows[row + 1].cells[cell].classList.contains('emptyhit')) {
                         table2.rows[row + 1].cells[cell].className = 'empty'
                     }
-                    if (row - 1 >= 0) {
+                    if (row - 1 >= 0 && !table2.rows[row - 1].cells[cell].classList.contains('emptyhit')) {
                         table2.rows[row - 1].cells[cell].className = 'empty'
                     }
-                    if (lastcell < 10) {
+                    if (lastcell < 10 && !table2.rows[row].cells[lastcell].classList.contains('emptyhit')) {
                         table2.rows[row].cells[lastcell].className = 'empty'
                     }
-                    if (firstcell >= 0) {
+                    if (firstcell >= 0 && !table2.rows[row].cells[firstcell].classList.contains('emptyhit')) {
                         table2.rows[row].cells[firstcell].className = 'empty'
                     }
-                    if (row - 1 >= 0 && lastcell < 10) {
+                    if (row - 1 >= 0 && lastcell < 10 && !table2.rows[row - 1].cells[lastcell].classList.contains('emptyhit')) {
                         table2.rows[row - 1].cells[lastcell].className = 'empty'
                     }
-                    if (row + 1 < 10 && lastcell < 10) {
+                    if (row + 1 < 10 && lastcell < 10 && !table2.rows[row + 1].cells[lastcell].classList.contains('emptyhit')) {
                         table2.rows[row + 1].cells[lastcell].className = 'empty'
                     }
-                    if (row - 1 >= 0 && firstcell >= 0) {
+                    if (row - 1 >= 0 && firstcell >= 0 && !table2.rows[row - 1].cells[firstcell].classList.contains('emptyhit')) {
                         table2.rows[row - 1].cells[firstcell].className = 'empty'
                     }
-                    if (row + 1 < 10 && firstcell >= 0) {
+                    if (row + 1 < 10 && firstcell >= 0 && !table2.rows[row + 1].cells[firstcell].classList.contains('emptyhit')) {
                         table2.rows[row + 1].cells[firstcell].className = 'empty'
                     }
                 }
             }
-
             shipsLocation.splice(i, 1)
             shipThink()
         }
@@ -176,7 +180,7 @@ socket.on('move', (msg) => {
     } else {
         table2stop.style.width = '0%'
         table1stop.style.width = '100%'
-        table2.style.boxShadow = '0 0 20px green'
+        table2.style.boxShadow = '0 0 20px red'
         table1.style.boxShadow = ''
     }
 });
@@ -213,6 +217,7 @@ socket.on('ready', (d) => {
 socket.on('end', (endData) => {
     gg = true
     if (endData === 'gg') {
+        document.getElementById('loose').play()
         document.getElementById('table1stop').style.width = '100%'
         table2stop.style.width = '100%'
         document.getElementById('gamestate').style.color = 'red'
@@ -220,13 +225,13 @@ socket.on('end', (endData) => {
         document.getElementById('gamestate').style.display = 'inline'
         socket.emit('end', 'win')
     } else if (endData === 'win') {
+        document.getElementById('win').play()
         document.getElementById('table1stop').style.width = '100%'
         table2stop.style.width = '100%'
         document.getElementById('gamestate').style.color = 'green'
         document.getElementById('gamestate').textContent = 'You Win'
         document.getElementById('gamestate').style.display = 'inline'
     } else {
-        console.log(endData);
         document.getElementById('table1stop').style.width = '100%'
         table2stop.style.width = '100%'
         document.getElementById('gamestate').textContent = 'Opponent left'
@@ -238,11 +243,13 @@ socket.on('shot', function (hittData) {
     let row = hittData[0]
     let cell = hittData[2]
     let count = 0
+    shotSound()
     if (player1Data.coords[row][cell] === 1) {
         table1.rows[row].cells[cell].className = 'kill'
         player1Data.coords[row][cell] = 'x'
+
     } else {
-        table1.rows[row].cells[cell].className = 'empty'
+        table1.rows[row].cells[cell].className = 'emptyhit'
     }
     for (let i = 0; i < player1Data.shipsLocation.length; i++) {
         let ships = Object.values(player1Data.shipsLocation[i])
@@ -266,58 +273,59 @@ socket.on('shot', function (hittData) {
                 let firstcell = +ship[0][2] - 1
                 table1.rows[row].cells[cell].style.backgroundColor = 'rgba(0, 128, 0, 0.5)'
                 if (position) {
-                    if (cell + 1 < 10) {
+                    if (cell + 1 < 10 && !table1.rows[row].cells[cell + 1].classList.contains('emptyhit')) {
                         table1.rows[row].cells[cell + 1].className = 'empty'
                     }
-                    if (cell - 1 >= 0) {
+                    if (cell - 1 >= 0 && !table1.rows[row].cells[cell - 1].classList.contains('emptyhit')) {
                         table1.rows[row].cells[cell - 1].className = 'empty'
                     }
-                    if (lastrow < 10) {
+                    if (lastrow < 10 && !table1.rows[lastrow].cells[cell].classList.contains('emptyhit')) {
                         table1.rows[lastrow].cells[cell].className = 'empty'
                     }
-                    if (firstrow >= 0) {
+                    if (firstrow >= 0 && !table1.rows[firstrow].cells[cell].classList.contains('emptyhit')) {
                         table1.rows[firstrow].cells[cell].className = 'empty'
                     }
-                    if (cell - 1 >= 0 && lastrow < 10) {
+                    if (cell - 1 >= 0 && lastrow < 10 && !table1.rows[lastrow].cells[cell - 1].classList.contains('emptyhit')) {
                         table1.rows[lastrow].cells[cell - 1].className = 'empty'
                     }
-                    if (cell + 1 < 10 && lastrow < 10) {
+                    if (cell + 1 < 10 && lastrow < 10 && !table1.rows[lastrow].cells[cell + 1].classList.contains('emptyhit')) {
                         table1.rows[lastrow].cells[cell + 1].className = 'empty'
                     }
-                    if (cell - 1 >= 0 && firstrow >= 0) {
+                    if (cell - 1 >= 0 && firstrow >= 0 && !table1.rows[firstrow].cells[cell - 1].classList.contains('emptyhit')) {
                         table1.rows[firstrow].cells[cell - 1].className = 'empty'
                     }
-                    if (cell + 1 < 10 && firstrow >= 0) {
+                    if (cell + 1 < 10 && firstrow >= 0 && !table1.rows[firstrow].cells[cell + 1].classList.contains('emptyhit')) {
                         table1.rows[firstrow].cells[cell + 1].className = 'empty'
                     }
                 } else {
-                    if (row + 1 < 10) {
+                    if (row + 1 < 10 && !table1.rows[row + 1].cells[cell].classList.contains('emptyhit')) {
                         table1.rows[row + 1].cells[cell].className = 'empty'
                     }
-                    if (row - 1 >= 0) {
+                    if (row - 1 >= 0 && !table1.rows[row - 1].cells[cell].classList.contains('emptyhit')) {
                         table1.rows[row - 1].cells[cell].className = 'empty'
                     }
-                    if (lastcell < 10) {
+                    if (lastcell < 10 && !table1.rows[row].cells[lastcell].classList.contains('emptyhit')) {
                         table1.rows[row].cells[lastcell].className = 'empty'
                     }
-                    if (firstcell >= 0) {
+                    if (firstcell >= 0 && !table1.rows[row].cells[firstcell].classList.contains('emptyhit')) {
                         table1.rows[row].cells[firstcell].className = 'empty'
                     }
-                    if (row - 1 >= 0 && lastcell < 10) {
+                    if (row - 1 >= 0 && lastcell < 10 && !table1.rows[row - 1].cells[lastcell].classList.contains('emptyhit')) {
                         table1.rows[row - 1].cells[lastcell].className = 'empty'
                     }
-                    if (row + 1 < 10 && lastcell < 10) {
+                    if (row + 1 < 10 && lastcell < 10 && !table1.rows[row + 1].cells[lastcell].classList.contains('emptyhit')) {
                         table1.rows[row + 1].cells[lastcell].className = 'empty'
                     }
-                    if (row - 1 >= 0 && firstcell >= 0) {
+                    if (row - 1 >= 0 && firstcell >= 0 && !table1.rows[row - 1].cells[firstcell].classList.contains('emptyhit')) {
                         table1.rows[row - 1].cells[firstcell].className = 'empty'
                     }
-                    if (row + 1 < 10 && firstcell >= 0) {
+                    if (row + 1 < 10 && firstcell >= 0 && !table1.rows[row + 1].cells[firstcell].classList.contains('emptyhit')) {
                         table1.rows[row + 1].cells[firstcell].className = 'empty'
                     }
                 }
             }
             player1Data.shipsLocation.splice(i, 1)
+            shipThink()
         }
         count = 0
     }
